429244ce4cd4d3e9cc7f8eb89f71d2667317ad44
zhenjiehan <446623911@qq.com>
2021-08-04 11:37:03 +0800

    完成登录界面 图片验证码 生成功能

--- src/main/java/com/zhenjiehan/community/controller/LoginController.java
+++ src/main/java/com/zhenjiehan/community/controller/LoginController.java
@@ -1,8 +1,12 @@
 package com.zhenjiehan.community.controller;
 
+import com.google.code.kaptcha.Producer;
+import com.sun.deploy.net.HttpResponse;
 import com.zhenjiehan.community.entity.User;
 import com.zhenjiehan.community.service.UserService;
 import com.zhenjiehan.community.utils.CommunityConstant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,13 +14,25 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Map;
 
 @Controller
 public class LoginController implements CommunityConstant {
 
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+
     @Autowired
-    UserService userService;
+    private UserService userService;
+
+    @Autowired
+    private Producer kaptchaProducer;
 
     @RequestMapping(path = "/register", method = RequestMethod.GET)
     public String getRegisterPage() {
@@ -59,4 +75,21 @@ public class LoginController implements CommunityConstant {
         return "/site/operate-result";
     }
 
+    @RequestMapping(path = "/kaptcha", method = RequestMethod.GET)
+    public void getKaptcha(HttpServletResponse response, HttpSession session) {
+        String text = kaptchaProducer.createText();
+        BufferedImage image = kaptchaProducer.createImage(text);
+
+        //将验证码存入session
+        session.setAttribute("kaptcha", text);
+
+        //将图片输出给浏览器
+        response.setContentType("image/png");
+        try {
+            OutputStream os = response.getOutputStream();
+            ImageIO.write(image, "png", os);
+        } catch (IOException e) {
+            logger.error("相应验证码失败!" + e.getMessage());
+        }
+    }
 }
